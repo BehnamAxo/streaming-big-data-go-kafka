9f2c93d48f1f5841d81d3b90257aa22f09b45e9b
Behnam Amiri <behamiridev@gmail.com>
2025-04-21 12:20:35 -0700

    Add Kafdrop

--- consumer/main.go
+++ consumer/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	r := kafka.NewReader(kafka.ReaderConfig{
+	reader := kafka.NewReader(kafka.ReaderConfig{
 		Brokers:   []string{"localhost:9092"},
 		Topic:     "logs",
 		GroupID:   "log-consumers",
@@ -17,13 +17,14 @@ func main() {
 		MinBytes:  10e3,
 		MaxBytes:  10e6,
 	})
-	defer r.Close()
+
+	defer reader.Close()
 
 	count := 0
 	start := time.Now()
 
 	for {
-		_, err := r.ReadMessage(context.Background())
+		_, err := reader.ReadMessage(context.Background())
 		if err != nil {
 			fmt.Println("Error reading message:", err)
 			break
